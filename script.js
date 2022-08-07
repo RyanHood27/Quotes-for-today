61c93de44ccd1ed9c8e06fdda7de2f2d44829713
RyanHood27 <hoodryan782@gmail.com>
2022-08-07 16:16:11 +0800

    first commit

---
+++ script.js
@@ -0,0 +1,43 @@
+const quote = document.querySelector(".js-quote"),
+  authorName = document.querySelector(".author .name"),
+  spinner = document.querySelector("#js-spinner"),
+  copyBtn = document.querySelector(".js-copy"),
+  volumeBtn = document.querySelector(".js-volume"),
+  generateBtn = document.querySelector("button"),
+  randomQuoteAPI = "https://api.quotable.io/random";
+
+const fetchRandomQuote = async () => {
+  spinner.classList.remove("hidden");
+  generateBtn.classList.add("loading");
+
+  try {
+    const response = await fetch(randomQuoteAPI);
+    if (!response.ok) {
+      throw Error(response.statusText);
+    }
+    const result = await response.json();
+    quote.textContent = result.content;
+    authorName.textContent = result.author;
+  } catch {
+    alert("Failed! Please open data connection.");
+  } finally {
+    spinner.classList.add("hidden");
+    generateBtn.classList.remove("loading");
+  }
+};
+
+generateBtn.addEventListener("click", fetchRandomQuote);
+
+copyBtn.addEventListener("click", () => {
+  navigator.clipboard.writeText(quote.textContent);
+});
+
+volumeBtn.addEventListener("click", () => {
+  let utterance = new SpeechSynthesisUtterance(
+    `${quote.textContent} by ${authorName.textContent}`
+  );
+
+  speechSynthesis.speak(utterance);
+});
+
+fetchRandomQuote();
